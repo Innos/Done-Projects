60cf6212d96124dece49b744380f5f36a40ce197
Innos <victor.d.kazakov@gmail.com>
2016-03-21 00:26:09 +0200

    added HTML Teamwork

---
+++ TeamworkHTML/js/main.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function() {
+    $('.navigation-button').click(function() {
+        var caller = this.id;
+        var elems = document.getElementsByClassName('content');
+        var previous = "#" + elems[0].id;
+        var id = "#" + caller + "-page"; 
+        $(previous).animate({
+            left: '-500%'
+
+        },
+        {
+            duration: 500,
+            easing: 'linear',
+            complete: function() {
+                $(previous).css('left', '200%');
+                $(previous).appendTo('#wrapper');
+            }
+        });
+
+        $(id).animate({
+            left: '0%'
+        }, 500);
+
+        $(previous).removeClass('content');
+        $(previous).addClass('hidden');
+        $(id).removeClass('hidden');
+        $(id).addClass('content');
+    });
+});
